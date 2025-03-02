93789da2dc7ceeaa4755bfac0bc17331f9adade5
Harsh Joshi <107912873+harshjoshiiii@users.noreply.github.com>
2025-03-02 13:59:48 +0530

    Time: 3 ms (82.87%), Space: 19.9 MB (91.15%) - LeetHub

---
+++ 2363-merge-similar-items/2363-merge-similar-items.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> mergeSimilarItems(vector<vector<int>>& nums1, vector<vector<int>>& nums2)
+    {
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());        
+        int i=0;
+        int j=0;
+        int n=nums1.size(),m=nums2.size();
+        vector<vector<int>>ans;
+        while(i < n && j <m)
+        {
+            if(nums1[i][0]==nums2[j][0])
+            {
+                ans.push_back({nums1[i][0],nums1[i][1]+nums2[j][1]});
+                i++;
+                j++;
+            }
+            else if (nums1[i][0] < nums2[j][0])
+            {
+                ans.push_back(nums1[i]);
+                i++;
+            }
+            else {
+                ans.push_back(nums2[j]);
+                j++;
+            }
+        }
+        //remaining of nums1
+        while (i < n) {
+            ans.push_back(nums1[i]);
+            i++;
+        }
+        //remaining of nums2
+        while (j < m) {
+            ans.push_back(nums2[j]);
+            j++;
+        }
+    return ans;
+    }
+};
