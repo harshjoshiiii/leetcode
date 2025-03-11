a82f062cf7f86585189fbd99a73c4fb04787a558
Harsh Joshi <107912873+harshjoshiiii@users.noreply.github.com>
2025-03-12 00:43:25 +0530

    Time: 20 ms (31.92%), Space: 9.8 MB (100%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -3,20 +3,15 @@ public:
     int numberOfSubstrings(string s) {
         int n = s.length();
         int cnt=0;
-        for(int i=0;i<n;i++)
-        {
-            set<char>st;
-            for(int j=i;j<n;j++)
-            {
-                st.insert(s[j]);
-                if (st.size() == 3) {  // Check for exactly 3 distinct characters
-                    cnt++;  // Valid substring found
-                }
-            }
-            if (st.size() > 3) {  
-                break;
+        vector<int> lastSeen(3, -1); // Vector to track the last seen positions of 'a', 'b', and 'c'
+        for (int i = 0; i < n; i++) {
+            lastSeen[s[i] - 'a'] = i;
+            if (lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1) {
+                // Find the minimum index among the last seen positions of 'a', 'b', and 'c'
+                int minLastSeen = min(lastSeen[0], min(lastSeen[1], lastSeen[2]));
+                cnt += minLastSeen + 1;
             }
         }
-    return cnt;
+        return cnt;
     }
 };
